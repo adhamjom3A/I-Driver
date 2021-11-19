42b6f9ee2d6a063fd14afdaf2b3da35abc46215e
mohamed abderlaziz <57459150+lovren43@users.noreply.github.com>
2021-11-20 00:22:56 +0200

    Update User.java

--- User.java
+++ User.java
@@ -1,21 +1,32 @@
 package company;
 
 public class User extends Person {
-    private String Source;
-    private  String Destination;
+	private String Source;
+	private String Destination;
 
+	public Boolean isValid(personInfo personInfo) {
+		super.isValid(personInfo);
+		personInfo.setCurrentStatus(Status.valueOf("Active"));
+		return true;
+	}
 
-    public Boolean isValid(personInfo personInfo){
-        super.isValid(personInfo);
-        personInfo.setCurrentStatus(Status.valueOf("Active"));
-        return true;
-    }
+	public Ride Request(String source, String destination) {
+		Ride r = new RideRequest();
+		r.setDestination(destination);
+		r.setSource(source);
+		r.setUser(this);
+		return r;
+	}
 
-    public RideRequest Request(String source, String destination){
-        RideRequest r=new RideRequest();
-        r.setDestination(destination);
-        r.setSource(source);
-        r.setUser(this);
-        return r;
-    }
+	public boolean acceptOffer(Driver driver) {
+		System.out.println("The Driver says this ride will cost " + driver.getAvailbleRide().getPrice());
+		System.out.println("y or n");
+		Scanner sc = new Scanner(System.in);
+		char input = sc.nextchar();
+		if (input == "y") {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }

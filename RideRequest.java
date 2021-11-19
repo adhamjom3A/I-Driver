f9ec4babf0293c44a2f8c47f25d38d91633b8066
mohamed abderlaziz <57459150+lovren43@users.noreply.github.com>
2021-11-20 00:22:12 +0200

    Update RideRequest.java

--- RideRequest.java
+++ RideRequest.java
@@ -1,35 +1,19 @@
 package company;
 
-public class RideRequest {
-    private String Source;
-    private String Destination;
-    private User user;
-
-    public String getSource() {
-        return Source;
-    }
-
-    public void setSource(String source) {
-        Source = source;
-    }
-
-    public String getDestination() {
-        return Destination;
-    }
-
-    public void setDestination(String destination) {
-        Destination = destination;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-    public String notification(Driver driver)
-    {
-        return "there's a ride";
-    }
+public class RideOperations {
+	public void assignDriverToRide(arrayLists inventory) {
+		for (int request = 0; request < inventory.RideRequest.size(); request++) {
+			for (int driver = 0; driver < inventory.driversList.size(); driver++) {
+				for (int favArea = 0; favArea < inventory.driversList.get(driver).getFavAreas().size(); favArea++) {
+					if (inventory.RideRequest.get(request).getDestination() == inventory.driversList.get(driver)
+							.getFavAreas().get(favArea)
+							&& (inventory.driversList.get(driver).getPersonInfo().getCurrentStatus()
+									.equals("ACTIVE"))) {
+						inventory.driversList.get(driver).setAvailbleRide(inventory.RideRequest.get(request));
+						inventory.driversList.get(driver).mkNotified();
+					}
+				}
+			}
+		}
+	}
 }
